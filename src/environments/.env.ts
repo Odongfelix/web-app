8db54aa2056d2559edc6771b27cbe974e15e370e
Odongfelix <91344900+Odongfelix@users.noreply.github.com>
2025-01-14 09:02:13 +0300

    run configurations

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '241204',
-    'hash': '29cda98b'
+    'version': '250113',
+    'hash': '3bdeeb82'
   },
   'allow_switching_backend_instance': true
 };
