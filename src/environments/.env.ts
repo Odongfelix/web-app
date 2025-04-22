9779ee9eb249ca60c733a531b52f0930d64b24c2
bravosty <stephenbravo58@gmail.com>
2025-04-22 17:51:13 +0300

    permissions for rate config

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '250414',
-    'hash': '4a241540'
+    'version': '250422',
+    'hash': '20300e13'
   },
   'allow_switching_backend_instance': true
 };
